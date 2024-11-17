8fbaea8d3248babb2c10e85b8bed137047b47d98
Zait Olzhas <ozait@scnsoft.com>
2024-11-17 21:08:52 +0500

    Attempt to remove unused junk from project

--- CryptoBank_WebApi/Program.cs
+++ CryptoBank_WebApi/Program.cs
@@ -1,44 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Primitives;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  
 }
 
-app.UseHttpsRedirection();
+//app.UseRouting();
 
-var summaries = new[]
+
+app.MapGet("/health", async (context) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    context.Response.Headers.Append("Content-Type", new StringValues("text/plain; charset=UTF-8"));
+    await context.Response.Body.WriteAsync("OK"u8.ToArray());
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
