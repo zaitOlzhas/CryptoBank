12c6d4c265d2631190848c12d6e7415147db444b
Zait Olzhas <ozait@scnsoft.com>
2024-11-28 09:16:20 +0500

    Ading Models and migrations

---
+++ CryptoBank_WebApi/Features/Auth/Domain/UserRefreshToken.cs
@@ -0,0 +1,9 @@
+namespace CryptoBank_WebApi.Features.Auth.Domain;
+
+public class UserRefreshToken
+{
+    public int Id { get; set; }
+    public string Token { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public User User { get; set; }
+}
