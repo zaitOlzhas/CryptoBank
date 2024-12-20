1f3a15f11cb4a142dfacce4ddc52bce7d19846b7
Zait Olzhas <ozait@scnsoft.com>
2024-12-20 08:06:22 +0500

    Account limit from configs

---
+++ CryptoBank_WebApi/Features/Account/Configurations/AccountConfigurations.cs
@@ -0,0 +1,6 @@
+namespace CryptoBank_WebApi.Features.Account.Configurations;
+
+public class AccountConfigurations
+{
+    public int AccountLimitPerUser { get; set; }
+}
--- CryptoBank_WebApi/Features/Account/Requests/CreateAccount.cs
+++ CryptoBank_WebApi/Features/Account/Requests/CreateAccount.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using CryptoBank_WebApi.Database;
+using CryptoBank_WebApi.Features.Account.Configurations;
 using CryptoBank_WebApi.Features.Account.Model;
 using CryptoBank_WebApi.Features.Auth.Model;
 using CryptoBank_WebApi.Migrations;
@@ -7,6 +8,7 @@ using FastEndpoints;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace CryptoBank_WebApi.Features.Account.Requests;
 
@@ -26,7 +28,8 @@ public class CreateAccount
     public record Request(ClaimsPrincipal Principal) : IRequest<Response>;
     public record Response(AccountModel account);
 
-    public class RequestHandler(CryptoBank_DbContext dbContext) : IRequestHandler<Request, Response>
+    public class RequestHandler(CryptoBank_DbContext dbContext, IOptions<AccountConfigurations> authConfigs)
+        : IRequestHandler<Request, Response>
     {
         public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
         {
@@ -37,12 +40,23 @@ public class CreateAccount
             var user = await dbContext.Users
                 .Where(x => x.Email == email)
                 .SingleOrDefaultAsync(cancellationToken);
+            if (user is null)
+                throw new Exception("Invalid user");
+
+            var userAccounts = await dbContext.Accounts
+                .Where(x => x.UserId == user.Id)
+                .ToListAsync(cancellationToken);
+
+            if (userAccounts.Count >= authConfigs.Value.AccountLimitPerUser)
+                throw new Exception("Account limit reached");
+
             var account = new Domain.Account();
             account.Currency = "USD";
             account.UserId = user.Id;
 
-            var newAccount = dbContext.Accounts.Add(account);
-
+            var newAccount = await dbContext.Accounts.AddAsync(account, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            
             return new Response(new AccountModel(
                 newAccount.Entity.Number,
                 newAccount.Entity.Currency,
